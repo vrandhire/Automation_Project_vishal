22dbc5c51133aa9e785c5c24acd8a7d95312d15c
vrandhire <121479084+vrandhire@users.noreply.github.com>
2023-05-27 11:55:46 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -18,8 +18,23 @@ if ! systemctl is-enabled --quiet apache2; then
 fi
 
 
-timestamp=$(date '+%d%m%Y-%H%M%S')
-tar -cf /tmp/${myname}-httpd-logs-${timestamp}.tar.gz -C /var/log/apache2/ '*.log'
+inventory_file="/var/www/html/inventory.html"
 
+# Check if inventory file exists, create it if not
+if [[ ! -f "$inventory_file" ]]; then
+    echo -e "Log Type\tDate Created\tType\tSize" > "$inventory_file"
+fi
+
+
+timestamp=$(date '+%d%m%Y-%H%M%S')
+
+tar_file="/tmp/${myname}-httpd-logs-${timestamp}.tar.gz"
+tar -czf "$tar_file" -C /var/log/apache2/ --exclude='*.zip' --exclude='*.tar' '*.log'
 
 aws s3 cp /tmp/${myname}-httpd-logs-${timestamp}.tar.gz s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar.gz
+
+archive_size=$(du -h "$tar_file" | cut -f1)
+
+log_type=$(basename "$tar_file" | cut -d'-' -f2)
+
+echo -e "${log_type}\t${timestamp}\ttar\t${archive_size}" >> "$inventory_file"
